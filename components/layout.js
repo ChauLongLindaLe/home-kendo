7bde93a24255d3b0543ffcac28cb4aa086086593
Alex Blair <alex.blair@fundingcircle.com>
2020-07-04 14:57:04 +0100

    Fixup layout component

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,21 @@
-import styles from "./layout.module.css";
 import Head from "next/head";
+import styles from "./layout.module.css";
 
-// export default function Layout({ children }) {
-//     return <div className={styles.container}>{children}</div>
-// }
-
-const Layout = (props) => (
-  <div>
+const Layout = ({ children }) => (
+  <div className={styles.container}>
     <Head>
       <title>Kendo</title>
       <link rel="icon" href="/favicon.ico" />
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css"
-      ></link>
+      />
       <script
         defer
         src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"
-      ></script>
+      />
     </Head>
-    <div className="section">{props.children}</div>
+    <main>{children}</main>
   </div>
 );
 
